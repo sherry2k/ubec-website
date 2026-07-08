f6efae15f3420d55b3db1d12efd8917f1aa70067
sherry2k <131241296+sherry2k@users.noreply.github.com>
2026-07-08 13:47:13 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -116,8 +116,7 @@ export default function Contact() {
 
             {/* Map */}
             <div className="rounded-2xl overflow-hidden border border-border/40 h-48">
-              <iframe
-                src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4307.9891326894885!2d54.37756336821889!3d24.459023354077484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5e67c6c53963b9%3A0xc0cd3c6978682d1f!2suniversal%20building%20engineering%20consultants!5e1!3m2!1sen!2sae!4v1783503828470!5m2!1sen!2sae" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin>"
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4307.9891326894885!2d54.37756336821889!3d24.459023354077484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5e67c6c53963b9%3A0xc0cd3c6978682d1f!2suniversal%20building%20engineering%20consultants!5e1!3m2!1sen!2sae!4v1783503950280!5m2!1sen!2sae" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
